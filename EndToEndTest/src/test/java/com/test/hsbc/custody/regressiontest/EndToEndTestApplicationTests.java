5ef05d33118eb3b9ac04f38e168bb176654a2b43
mahesh.patkulkar <mahesh.patkulkar@Mahesh>
2021-05-03 18:28:56 +0530

    End to end testing

---
+++ EndToEndTest/src/test/java/com/test/hsbc/custody/regressiontest/EndToEndTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.hsbc.custody.regressiontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndToEndTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
